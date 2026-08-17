ae424da9093f352bf9d481bee5ad03be5d4fc72a
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-08-18 00:25:10 +0500

    layout(sidebar): add sticky pinned top-0 layout behavior to desktop sidebar

--- components/layout/AppSidebar.tsx
+++ components/layout/AppSidebar.tsx
@@ -73,7 +73,7 @@ export function AppSidebar({
     <aside
       aria-label="Application sidebar"
       className={cn(
-        'hidden h-dvh w-64 shrink-0 flex-col border-r border-border bg-background-light lg:flex',
+        'hidden sticky top-0 h-dvh w-64 shrink-0 flex-col border-r border-border bg-background-light lg:flex',
         className
       )}
     >
